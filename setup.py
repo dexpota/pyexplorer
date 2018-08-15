f0f4b3ab7ffd6dd3f16e737a7ca253960481177e
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-08-15 11:52:36 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyexplorer",
-    version="0.1",
+    version="0.1a0",
     author="Fabrizio Destro",
     author_email="destro.fabrizio@gmail.com",
-    entry_points= {
+    entry_points={
         'console_scripts': ["pyexplorer=pyexplorer.pyexplorer:main"]
     },
     description="Explore python modules interactively.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2"
+    ],
+    install_requires=[
+        "termcolor"
+    ],
     url="https://github.com/dexpota/pyexplorer",
     packages=setuptools.find_packages(),
 )
