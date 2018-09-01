27d5b33be61da3d8b8f99f4eec7b55aed958156f
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-09-01 17:20:52 +0200

    Trying prompt_toolkit.

--- pyexplorer/pyexplorer.py
+++ pyexplorer/pyexplorer.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
 import os
 from glob import glob
-from argparse import ArgumentParser
 from importlib import import_module
 from termcolor import colored
 import inspect
 import types
-import __builtin__
-from functools import partial
 
 
 def module_format(contents):
@@ -121,58 +117,4 @@ def method_filter(item, _class, depth=0):
 def dir_filter(item):
     return not item.startswith("_")
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("module", help="Module or package name.", type=str)
-    parser.add_argument("-a", action="store_true", help="Show everything inside the module/package.")
-    parser.add_argument("--level", type=int, default=0, help="List all methods inherited up to this level.")
-    parser.add_argument("--debug", action="store_true", default=False, help="Debug mode.")
-    args = parser.parse_args()
 
-    filters = []
-    if not args.a:
-        filters.append(dir_filter)
-
-    for i in range(args.module.count(".") + 1):
-        try:
-            module_package_name = args.module.rsplit('.', i)[0]
-            _ = import_module(module_package_name)
-            local_path = ".".join(args.module.rsplit('.', i)[1:])
-            break
-        except ImportError:
-            module_package_name = ""
-            local_path = ""
-
-    if args.debug:
-        assert ".".join(filter(lambda s: s != "", [module_package_name, local_path])) == args.module
-
-    if not module_package_name:
-        attribute = getattr(__builtin__, args.module)
-        c = process_attribute(attribute, lambda x: all([f(x) for f in filters]))
-        formatter = attribute_format
-    else:
-        pass # try import global_path
-        module_package = import_module(module_package_name)
-        if not local_path:
-            module_package = import_module(module_package_name)
-
-            if hasattr(module_package, "__path__"):
-                c = extract_package(module_package)
-            else:
-                c = extract(module_package, lambda x: all([f(x) for f in filters]))
-
-            formatter = module_format
-        else:
-            parent_scope = module_package
-            for i in range(local_path.count(".") + 1):
-                attribute_name = local_path.split(".")[i]
-                attribute = getattr(parent_scope, attribute_name)
-                parent_scope = attribute
-
-            c = process_attribute(attribute, lambda x: all([f(x) for f in filters]))
-            formatter = attribute_format
-
-    formatter(c)
-
-if __name__ == "__main__":
-    main()
