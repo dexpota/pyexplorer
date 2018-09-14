7869ec2089c4378bf9198a459d7fb2bb007b5455
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-09-14 20:56:52 +0200

    Fixed assertion.

--- pyexplorer/utilities.py
+++ pyexplorer/utilities.py
@@ -3,7 +3,9 @@ from importlib import import_module
 
 def find_innermost_module(full_qualifier):
     """
-    This function find the innermost module or package specified inside module_path.
+    This function find the innermost module or package specified inside module_path, it does this by trying importing
+    full_qualifier and the stripping the last name from it and trying again. If the qualifier is a builtin then it
+    returns two empty strings.
 
     >>> find_innermost_module("logging.config.fileConfig")
     ('logging.config', 'fileConfig')
@@ -14,10 +16,13 @@ def find_innermost_module(full_qualifier):
     >>> find_innermost_module("logging")
     ('logging', '')
 
+    >>> find_innermost_module("filter")
+    ('', '')
+
     :type full_qualifier: str
     :param full_qualifier: A string representing a package, a module or an attribute inside one of them.
     :return: A tuple where the first element is the module or package name and the second element is an empty string or
-    an the attribute name
+    an the attribute name. If full_qualifier is a builtin then both values are empty
     """
 
     module_package = ""
@@ -33,6 +38,8 @@ def find_innermost_module(full_qualifier):
             module_package = ""
             attribute = ""
 
-    assert module_package + ("." + attribute if attribute != "" else "") == full_qualifier
+    if module_package != "":
+        # When full_qualifier is a builtin then we don't have any value set.
+        assert module_package + ("." + attribute if attribute != "" else "") == full_qualifier
 
     return module_package, attribute
